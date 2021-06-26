6d2fbf1e4422be560114913d88ebb58daf8afecc
Duncan Proctor <dbp19a@my.fsu.edu>
2021-06-26 02:15:59 -0400

    cleanup

--- src/main/java/com/duncpro/rds/data/StreamUtil.java
+++ src/main/java/com/duncpro/rds/data/StreamUtil.java
@@ -7,15 +7,11 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class StreamUtil {
-    @SuppressWarnings("UnnecessaryLocalVariable")
     public static <T> Stream<T> unwrapStream(CompletableFuture<Stream<T>> streamFuture) {
         final Supplier<Spliterator<T>> supplier
                 = () -> streamFuture.join().spliterator();
 
-        final Stream<T> rsStream = StreamSupport
-                .stream(supplier, Spliterator.ORDERED | Spliterator.SIZED | Spliterator.SUBSIZED | Spliterator.IMMUTABLE,
-                        false);
-
-        return rsStream;
+        return StreamSupport.stream(supplier,
+                Spliterator.ORDERED | Spliterator.SIZED | Spliterator.SUBSIZED | Spliterator.IMMUTABLE, false);
     }
 }
