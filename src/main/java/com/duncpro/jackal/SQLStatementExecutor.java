0410d838fbe973f09f74a193ea9630093e3fbd24
Duncan Proctor <dbp19a@my.fsu.edu>
2021-06-29 13:22:52 -0400

    remove comment

--- src/main/java/com/duncpro/jackal/SQLStatementExecutor.java
+++ src/main/java/com/duncpro/jackal/SQLStatementExecutor.java
@@ -6,9 +6,5 @@ import javax.annotation.concurrent.ThreadSafe;
 
 @ThreadSafe
 interface SQLStatementExecutor {
-    /**
-     * Creates a new standalone statement which is automatically committed upon completion.
-     * @param parameterizedSQL a JDBC-like parameterized SQL query.
-     */
     StatementBuilder prepareStatement(@Language("SQL") String parameterizedSQL);
 }
