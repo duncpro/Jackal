a2a682a3b0501740b4246c9e37b99d3f35c82846
Duncan Proctor <dbp19a@my.fsu.edu>
2021-10-21 23:56:04 -0400

    specify behavior of QueryResultRow:#get

--- src/main/java/com/duncpro/jackal/MapResultRow.java
+++ src/main/java/com/duncpro/jackal/MapResultRow.java
@@ -12,6 +12,9 @@ public class MapResultRow implements QueryResultRow {
 
     @Override
     public <T> Optional<T> get(String columnName, Class<T> javaType) {
+        if (!columnValueMap.containsKey(columnName)) throw new IllegalArgumentException("No column by the name of" +
+                " \"" + columnName + "\" exists within this row.");
+
         final var rawValue = columnValueMap.get(columnName);
         if (rawValue == null) return Optional.empty();
         final T casted;
--- src/main/java/com/duncpro/jackal/QueryResultRow.java
+++ src/main/java/com/duncpro/jackal/QueryResultRow.java
@@ -6,6 +6,11 @@ import java.util.Optional;
 
 @ThreadSafe
 public interface QueryResultRow {
+    /**
+     * Returns an optional representing the value stored in the given column of this row.
+     * This function will throw an {@link IllegalArgumentException} if the given column name does
+     * not exist within the row.
+     */
     <T> Optional<T> get(String columnName, Class<T> javaType);
 
     static QueryResultRow fromMap(Map<String, Object> map) {
