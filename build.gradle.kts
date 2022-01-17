3983eda133d75e40c1af21f1bd58e83cb15e1a3a
Duncan Proctor <dbp19a@my.fsu.edu>
2022-01-16 21:20:29 -0500

    clarify

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.duncpro"
-version = "1.0-SNAPSHOT-"
+version = "1.0-SNAPSHOT-15"
 
 val intTest: SourceSet by sourceSets.creating {
     compileClasspath += sourceSets.main.get().output
